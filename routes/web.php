e991e71082291e2500d4aece1062aedd79aa25ae
remindbg <remindbg@gmail.com>
2017-04-20 14:13:01 +0300

    Added View Files

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.home');
+});
+
+Route::get('profile/{id?}', function ($id = null) {
+	return view('layouts.profile');
+});
+
+
+Route::get('videos/{genrename?}', function ($genrename = null) {
+	return view('layouts.videos');
+});
+
+
+Route::resource('users', 'UsersController', [
+    'only' => ['index', 'show']
+]);
+
+Route::get('lyrics', function () {
+    return view('layouts.lyrics');
+});
+
+Route::get('admin', function () {
+    return view('admin.main-admin');
+});
+
+Route::get('def', function () {
+    return view('layouts.default');
+});
+
+Route::get('news', function () {
+    return view('layouts.news');
 });
